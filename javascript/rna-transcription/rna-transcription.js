baa2cab20fc2909e52c59bb67d8b17fe0ea5c7f7
minnypatel <minnypatel@googlemail.com>
2018-12-18 16:52:01 +0000

    add rna-transcription mvp solution

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,10 +1,13 @@
 export const toRna = (dnaStrand) => {
-  const dnaArray = dnaStrand.split();
+  const dnaArray = dnaStrand.split('');
   const newArray = dnaArray.map((x) => {
     if (x === 'C') { return 'G'; }
     if (x === 'G') { return 'C'; }
+    if (x === 'T') { return 'A'; }
+    if (x === 'A') { return 'U'; }
+    throw new Error('Invalid input DNA.');
   });
 
-  const rnaStrang = newArray.join('');
-  return rnaStrang;
+  const rnaString = newArray.join('');
+  return rnaString;
 };
