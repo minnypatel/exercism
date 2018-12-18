3f9ff415b74b88bb865523e9ebbfb0d6f01520b2
minnypatel <minnypatel@googlemail.com>
2018-12-18 17:21:57 +0000

    add rna-transcription improved solution

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,13 +1,22 @@
 export const toRna = (dnaStrand) => {
+  const transcribeDnaMap = new Map([
+    ['C', 'G'],
+    ['G', 'C'],
+    ['T', 'A'],
+    ['A', 'U'],
+  ]);
+
   const dnaArray = dnaStrand.split('');
-  const newArray = dnaArray.map((x) => {
-    if (x === 'C') { return 'G'; }
-    if (x === 'G') { return 'C'; }
-    if (x === 'T') { return 'A'; }
-    if (x === 'A') { return 'U'; }
-    throw new Error('Invalid input DNA.');
+
+  const rnaArray = dnaArray.map((x) => {
+    const rnaComplement = transcribeDnaMap.get(x);
+    if (rnaComplement === undefined) {
+      throw new Error('Invalid input DNA.');
+    }
+    return rnaComplement;
   });
 
-  const rnaString = newArray.join('');
+  const rnaString = rnaArray.join('');
+
   return rnaString;
 };
