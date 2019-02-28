59c2c76c76a79adc247d60daf8a389ea2beea819
minnypatel <minnypatel@googlemail.com>
2019-02-28 11:46:33 +0000

    add darts solution

--- javascript/darts/darts.js
+++ javascript/darts/darts.js
@@ -1,11 +1,13 @@
 export const solve = (x, y) => {
-  // swing and a miss
-  if (x > 10 || y > 10) {
-    return 0;
-  }
+  // if (!(typeof x === 'number' && typeof y === 'number')) return null;
+  // if (x > 10 || y > 10) return 0;
+  // if (x > 5 || y > 5) return 1;
+  // if (x > 1 || y > 1) return 5;
+  // return 10;
 
-  // inside the overall target
-  if (x <= 10 && y <= 10) {
-    return 1;
-  }
+  if (x <= 1 && y <= 1) return 10;
+  if (x <= 5 && y <= 5) return 5;
+  if (x <= 10 && y <= 10) return 1;
+  if (x > 10 || y > 10) return 0;
+  return null;
 };
