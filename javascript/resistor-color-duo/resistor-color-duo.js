3fb18b88f31827b98ca2802b76f96f2bbf4d294a
minnypatel <minnypatel@googlemail.com>
2019-12-23 12:49:35 +0000

    add resistor-color-duo solution

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,8 +1,16 @@
-//
-// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white"
+];
 
-export const decodedValue = () => {
-  throw new Error("Remove this statement and implement this function");
+export const decodedValue = ([color1, color2]) => {
+  return Number(`${COLORS.indexOf(color1)}${COLORS.indexOf(color2)}`)
 };
